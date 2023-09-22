5928f9a48076fe2f63625e04570e7c0357b0d4ea
Marcel Michau <me@marcelmichau.dev>
2023-09-22 11:21:49 +0200

    fix: use returned password cred from call to AddPassword + null handling

--- RotateAppRegSecretFunction.cs
+++ RotateAppRegSecretFunction.cs
@@ -10,46 +10,52 @@ using Microsoft.Graph;
 using Microsoft.Graph.Applications.Item.AddPassword;
 using Microsoft.Graph.Applications.Item.RemovePassword;
 
-namespace AppRegSecretRotation
-{
-    public class RotateAppRegSecretFunction
-    {
-        private readonly ILogger _logger;
+namespace AppRegSecretRotation;
 
-        public RotateAppRegSecretFunction(ILoggerFactory loggerFactory)
+public class RotateAppRegSecretFunction
+{
+    private readonly ILogger _logger;
+
+    public RotateAppRegSecretFunction(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<RotateAppRegSecretFunction>();
+    }
+
+    [Function(nameof(RotateAppRegSecretFunction))]
+    public async Task Run([EventGridTrigger] KeyVaultSecretNearExpirationEvent input)
+    {
+        _logger.LogInformation("Event Input Detail - Id: {Id} Topic: {Topic} Subject: {Subject} EventType: {EventType}", input.Id, input.Topic, input.Subject, input.EventType);
+
+        var appRegistrationObjectId = input.Subject;
+            
+        var keyVaultUri = Environment.GetEnvironmentVariable("KEY_VAULT_URI");
+
+        if (string.IsNullOrWhiteSpace(keyVaultUri))
         {
-            _logger = loggerFactory.CreateLogger<RotateAppRegSecretFunction>();
+            _logger.LogError("KEY_VAULT_URI environment variable not set");
+            return;
         }
 
-        [Function(nameof(RotateAppRegSecretFunction))]
-        public async Task Run([EventGridTrigger] KeyVaultSecretNearExpirationEvent input)
+        var secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
+
+        var previousClientSecret = await secretClient.GetSecretAsync(appRegistrationObjectId);
+
+        var previousClientSecretKeyId = new Guid(previousClientSecret.Value.Properties.ContentType);
+
+        _logger.LogInformation("Previous secret for app registration: {AppRegistrationObjectId} - Secret ID: {SecretId}", appRegistrationObjectId, previousClientSecretKeyId);
+
+        var newPasswordCredential = await SetAppRegistrationClientSecret(appRegistrationObjectId, previousClientSecretKeyId);
+
+        if (newPasswordCredential != null)
         {
-            _logger.LogInformation(input.Data.ToString());
-
-            _logger.LogInformation("Event Input Detail - Id: {Id} Topic: {Topic} Subject: {Subject} EventType: {EventType}", input.Id, input.Topic, input.Subject, input.EventType);
-
-            var appRegistrationObjectId = input.Subject;
-            
-            var keyVaultUri = Environment.GetEnvironmentVariable("KEY_VAULT_URI");
-
-            var secretClient = new SecretClient(new Uri(keyVaultUri), new DefaultAzureCredential());
-
-            var previousClientSecret = await secretClient.GetSecretAsync(appRegistrationObjectId);
-
-            var previousClientSecretKeyId = new Guid(previousClientSecret.Value.Properties.ContentType);
-
-            _logger.LogInformation("Previous secret for app registration: {AppRegistrationObjectId} - Secret ID: {SecretId}", appRegistrationObjectId, previousClientSecretKeyId);
-
-            var newPasswordCredential = await SetAppRegistrationClientSecret(appRegistrationObjectId, previousClientSecretKeyId);
-
             var newSecret = new KeyVaultSecret(appRegistrationObjectId, newPasswordCredential.SecretText)
+            {
+                Properties =
                 {
-                    Properties =
-                    {
-                        ContentType = newPasswordCredential.KeyId.ToString(),
-                        ExpiresOn = newPasswordCredential.EndDateTime
-                    }
-                };
+                    ContentType = newPasswordCredential.KeyId.ToString(),
+                    ExpiresOn = newPasswordCredential.EndDateTime
+                }
+            };
 
             try
             {
@@ -61,66 +67,72 @@ namespace AppRegSecretRotation
                 _logger.LogError(ex, "Error setting new secret for app registration: {AppRegistrationObjectId}", appRegistrationObjectId);
             }
         }
+    }
 
-        private async Task<PasswordCredential> SetAppRegistrationClientSecret(
-            string appRegistrationObjectId,
-            Guid previousClientSecretKeyId)
+    private async Task<PasswordCredential?> SetAppRegistrationClientSecret(
+        string appRegistrationObjectId,
+        Guid previousClientSecretKeyId)
+    {
+        try
         {
             var numberOfDaysUntilExpiry = Environment.GetEnvironmentVariable("NUMBER_OF_DAYS_UNTIL_EXPIRY") ?? "60";
 
             var graphServiceClient = new GraphServiceClient(
                 new DefaultAzureCredential(), new[] { "https://graph.microsoft.com/.default" });
 
-            var passwordCredential = new PasswordCredential
+            var initialPasswordCredential = new PasswordCredential
             {
                 DisplayName = "set-via-automation",
                 EndDateTime = DateTimeOffset.Now.AddDays(int.Parse(numberOfDaysUntilExpiry))
             };
 
-            try
+            var adApplication = await graphServiceClient.Applications[appRegistrationObjectId]
+                .GetAsync();
+
+            if (adApplication is null)
             {
-                _logger.LogInformation("Rotating client secret for app registration: {AppRegistrationObjectId}", appRegistrationObjectId);
-
-                var adApplication = await graphServiceClient.Applications[appRegistrationObjectId]
-                    .GetAsync();
-
-                _logger.LogInformation("Found app registration: {AppRegistrationDisplayName}", adApplication.DisplayName);
-
-                await graphServiceClient.Applications[appRegistrationObjectId]
-                    .AddPassword
-                    .PostAsync(new AddPasswordPostRequestBody
-                    {
-                        PasswordCredential = passwordCredential
-                    });
-
-                await graphServiceClient.Applications[appRegistrationObjectId]
-                    .RemovePassword
-                    .PostAsync(new RemovePasswordPostRequestBody
-                    {
-                        KeyId = previousClientSecretKeyId
-                    });
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Error rotating client secret for app registration: {AppRegistrationObjectId}", appRegistrationObjectId);
+                _logger.LogError("App registration not found: {AppRegistrationObjectId}", appRegistrationObjectId);
+                return null;
             }
 
-            return passwordCredential;
+            _logger.LogInformation("Rotating client secret for app registration: {AppRegistrationObjectId}", appRegistrationObjectId);
+
+            var finalPasswordCredential = await graphServiceClient.Applications[appRegistrationObjectId]
+                .AddPassword
+                .PostAsync(new AddPasswordPostRequestBody
+                {
+                    PasswordCredential = initialPasswordCredential
+                });
+
+            await graphServiceClient.Applications[appRegistrationObjectId]
+                .RemovePassword
+                .PostAsync(new RemovePasswordPostRequestBody
+                {
+                    KeyId = previousClientSecretKeyId
+                });
+
+            return finalPasswordCredential;
         }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error rotating client secret for app registration: {AppRegistrationObjectId}", appRegistrationObjectId);
+        }
+
+        return null;
     }
+}
 
-    public class KeyVaultSecretNearExpirationEvent
-    {
-        public string Id { get; set; }
+public class KeyVaultSecretNearExpirationEvent
+{
+    public string Id { get; set; }
 
-        public string Topic { get; set; }
+    public string Topic { get; set; }
 
-        public string Subject { get; set; }
+    public string Subject { get; set; }
 
-        public string EventType { get; set; }
+    public string EventType { get; set; }
 
-        public DateTime EventTime { get; set; }
+    public DateTime EventTime { get; set; }
 
-        public object Data { get; set; }
-    }
+    public object Data { get; set; }
 }
